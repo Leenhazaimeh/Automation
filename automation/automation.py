090ef6490297f9e9ec52103313a881727fa3fe99
Leen Hazaimeh <leenhazaimeh94@gmail.coom>
2021-09-06 20:21:14 +0300

    done

--- automation/automation.py
+++ automation/automation.py
@@ -1,58 +1,57 @@
-import shutil
-import re 
+import re
 from faker import Faker
 
+
 fake = Faker('en_US')
+'''''
+potential-contacts, find and collect all email addresses and phone numbers.
+phone numbers with missing area code should presume 206
+'''
 
 potential_contacts = ""
 
-existing_contacts = ""
-###########################
- #find and collect all email addresses and phone numbers.
- ###########################
-
-
-
-
-
-def get_e_mails(file):
-            
-            emails = []
-        
-if i[0] not in emails:
-            emails.append(i[0])
-return emails
-
-def get_phone_number(f:
-            
-            nums = []
-for i in range(100):
-
-    email = fake.email()
+for f in range(206):
+    email_text = fake.email()
     phone_number = fake.phone_number()
-if num not in nums:
-            nums.append(num)
+    potential_contacts += phone_number
+    potential_contacts += " " + email_text + " "
+  
+  
+   
+  
+    potential_contacts += "\n"
+with open("potential-contacts.txt", "w+") as f:
+    f.write(potential_contacts)
+# print(potential_contacts)
+
+
+
+phone_regex = list(re.findall(r"\w{3}-\w{3}-\w{4}", potential_contacts))
+
+email_regex = list(re.findall(r'[\w\.-]+@[\w\.-]+', potential_contacts))
+
+
+'''
+The ‘phone_numbers.txt’ and ‘emails.txt’ files will be verified by an automated system
+'''
+def phone(phone_regex):
+     return (('\n').join(phone_regex))
+phone_space=phone(phone_regex)
+
+def email(email_regex):
+     return (('\n').join(email_regex))
+email_space=email(email_regex)
+
+
+with open("phone_numbers.txt" , "w") as f:
+    f.write(phone_space)
+
+with open("email.txt" , "w") as f:
+    f.write(email_space)
 
 
 
 
+print(phone_regex) 
+print(email_regex)
 
-def auto(num, e_mails):
-
-    with open('phone_numbers.txt', 'w+')as p:
-        for x in num:
-            p.write(x + '\n')
-
-    with open('e_mails.txt', 'w+') as e: 
-        for n in e_mails: 
-            e.write(n + '\n')
-
-if __name__ == '__main__':
-    emails = get_emails(text)
-    phone_nums = get_phone_number(text)
-    save(phone_nums, emails)
-
-    print(phone_nums)
-    print(emails)
-with open('./assets/potential-contacts.txt', 'r') as file:
-    text = file.read().replace('\n', '')
